6892d109944ee6a50f9e933ddc9ef36c9ccfe756
Fahim Firoz <fahimfiroz.bd42@gmail.com>
2025-02-19 16:19:48 +0600

    overview page added

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,11 +1,126 @@
+import {  Carrot, DollarSign, House, ListOrdered, User } from "lucide-react";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
+const data = [
+  { name: "16 Jun", Received: 5000, Resolved: 3000, Unresolved: 2000 },
+  { name: "20 Jun", Received: 5500, Resolved: 3500, Unresolved: 2000 },
+  { name: "27 Jun", Received: 6000, Resolved: 4000, Unresolved: 2000 },
+  { name: "04 Jul", Received: 6200, Resolved: 4200, Unresolved: 2000 },
+  { name: "11 Jul", Received: 6500, Resolved: 4500, Unresolved: 2000 },
+];
 
 const Overview = () => {
+  const [date, setDate] = useState(new Date());
     return (
-        <div>
-            THis is overview
-            
+        <section>
+            <div className="bg-blue-500 px-2 py-4 rounded-xl flex justify-between items-center">
+                <h1 className="text-xl text-white font-bold ">Overview</h1>
+                
+
+            </div>
+
+            <div>
+      <div className='mt-12'>
+        {/* small cards */}
+        <div className='mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 flex-grow'>
+          {/* Sales Card */}
+          <div className='relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md'>
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-orange-600 to-orange-400 text-white shadow-orange-500/40`}
+            >
+              <DollarSign className='w-6 h-6 text-white' />
+            </div>
+            <div className='p-4 text-right'>
+              <p className='block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600'>
+                Total Revenue
+              </p>
+              <h4 className='block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900'>
+                $120
+              </h4>
+            </div>
+          </div>
+          {/* Total Orders */}
+          <div className='relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md'>
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-blue-600 to-blue-400 text-white shadow-blue-500/40`}
+            >
+              <ListOrdered className='w-6 h-6 text-white' />
+            </div>
+            <div className='p-4 text-right'>
+              <p className='block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600'>
+                Total Orders
+              </p>
+              <h4 className='block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900'>
+                120
+              </h4>
+            </div>
+          </div>
+          {/* Total Plants */}
+          <div className='relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md'>
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-pink-600 to-pink-400 text-white shadow-pink-500/40`}
+            >
+              <House className='w-6 h-6 text-white' />
+            </div>
+            <div className='p-4 text-right'>
+              <p className='block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600'>
+                Total Plants
+              </p>
+              <h4 className='block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900'>
+                120
+              </h4>
+            </div>
+          </div>
+          {/* Users Card */}
+          <div className='relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md'>
+            <div
+              className={`bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr shadow-lg absolute -mt-4 grid h-16 w-16 place-items-center from-green-600 to-green-400 text-white shadow-green-500/40`}
+            >
+              <User className='w-6 h-6 text-white' />
+            </div>
+            <div className='p-4 text-right'>
+              <p className='block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-600'>
+                Total User
+              </p>
+              <h4 className='block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-blue-gray-900'>
+                10
+              </h4>
+            </div>
+          </div>
         </div>
+
+        <div className='mb-4  grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3 '>
+          {/*Sales Bar Chart */}
+          <div className='relative flex flex-col bg-clip-border  rounded-xl bg-white text-gray-700 shadow-md overflow-hidden xl:col-span-2'>
+            {/* Chart goes here.. */}
+            <h1 className="text-2xl font-bold mt-5 ml-5">Sales</h1>
+            <ResponsiveContainer width="100%" height={300}>
+          <BarChart data={data}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Received" fill="#2196f3" />
+            <Bar dataKey="Resolved" fill="#4cc718" />
+            <Bar dataKey="Unresolved" fill="#f44336" />
+          </BarChart>
+        </ResponsiveContainer>
+          </div>
+          {/* Calender */}
+          <div className=' relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md overflow-hidden'>
+            <Calendar color='#4cc718'  onChange={setDate} value={date} className="w-full h-full rounded-xl"/>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+         
+            
+        </section>
     );
 };
 
